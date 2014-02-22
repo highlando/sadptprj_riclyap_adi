6ee0bdae2823942ae3f938de225a3bab6e02392a
highlando <jnhlnd@gmail.com>
2014-02-22 21:44:16 +0100

    mocks for build on rtd

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,13 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../'))
 
+# According to hint from
+# <http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/>.
+import mock
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.sparse', 'scipy.sparse.linalg']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
